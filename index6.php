960541c965c2964ec6b035cbdbce3a72c677fbc1
13rinda <brindarenekah@gmail.com>
2024-06-18 00:53:40 -0400

    theme change and footer

---
+++ index6.php
@@ -0,0 +1,236 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Contacts</title>
+    
+    <meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="img/favicon.ico" type="image/x-icon">
+    <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon" />
+    <meta name="description" content="Your description">
+    <meta name="keywords" content="Your keywords">
+    <meta name="author" content="Your name">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/responsive.css" type="text/css" media="screen">
+    <link rel="stylesheet" href="css/style.css" type="text/css" media="screen">
+    <link href='http://fonts.googleapis.com/css?family=Raleway:100' rel='stylesheet' type='text/css'>
+		<script type="text/javascript" src="js/jquery.js"></script>
+        <script type="text/javascript" src="js/jquery.easing.1.3.js"></script>
+        <script type="text/javascript" src="js/superfish.js"></script>
+        <script type="text/javascript" src="js/jquery.mobilemenu.js"></script>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-KS0EXRN6CP"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-KS0EXRN6CP');
+</script>
+        <script src="captcha.js"></script>
+        <style>
+          #captchaImage {
+              border: 1px solid #ccc;
+
+          }
+      </style>
+         
+        <script type="text/javascript" src="js/forms.js"></script>
+	<!--[if lt IE 8]>
+  		<div style='text-align:center'><a href="http://www.microsoft.com/windows/internet-explorer/default.aspx?ocid=ie6_countdown_bannercode"><img src="http://www.theie6countdown.com/img/upgrade.jpg"border="0"alt=""/></a></div>  
+ 	<![endif]-->
+  <!--[if lt IE 9]>
+     
+    <link rel="stylesheet" href="css/ie.css" type="text/css" media="screen">
+    <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+  <![endif]-->
+  
+</head>
+
+<body>
+<!--==============================header=================================-->
+<div class="container">
+  <div class="row">
+    <div class="span12">
+      <ul class="social-icons">
+        <li><a href="https://www.facebook.com/tamillydance"><img src="img/icon-1.png" alt=""></a></li>
+        <li><a href="https://x.com/i/flow/login?redirect_after_login=%2Ftamillydance"><img src="img/icon-2.png" alt=""></a></li>
+        <li><a href="https://youtube.com/@tamillydance"><img src="img/icon-3.png" alt=""></a></li>
+        <li><a href="https://www.tiktok.com/404?fromUrl=/tamillydance"><img src="img/icon-4.png" alt=""></a></li>
+        <li><a href="https://www.instagram.com/tamillydance/"><img src="img/icon-5.png" alt=""></a></li>
+    </ul>
+    </div>
+  </div>
+</div>          
+<div class="main">
+	<div class="main-bg">
+    <header>
+      <div class="container">
+        <div class="row">
+          <div class="span12 clearfix">
+            <h1 class="brand"><a href="index.html"><span>TAMILLY</span><strong>Hips don't lie, Tamilly sets the bars high!</strong></a></h1>
+          </div>
+        </div>
+      </div>
+      <div class="nav-border">
+          <div class="container">
+              <div class="row">
+                 <div class="col-lg-12">                                                 
+                  <ul class="nav justify-content-center">
+                    <li class="active"><a href="index.html">Main</a></li>
+                            
+                              <!--<ul>
+                              li><a href="#">Studio</a></li-->
+                                <!--li><a href="#">Teachers</a></li-->
+                                <!--li><a href="#">Dance styles</a></li>
+                                <li><a href="#">Schedule</a></li>
+                                <li><a href="#">Competitions</a></li>
+                                <li><a href="#">Gallery</a></li>
+                              </ul>-->
+                            </li>
+                            <!--li><a href="index-2.html">Teachers</a></li-->
+                            <li><a href="index-1.html">About</a></li>
+                            <li><a href="index-classes.html">Classes</a></li>
+                            <li><a href="index-3.html">Corporate</a></li>
+                            <li><a href="index-5.html">Gallery</a></li>
+                            <li><a href="index6.php">Hire A Belly Dancer</a></li>
+                            <li><a href="index-8.html">Contact</a></li>
+                  </ul>
+                  </div>
+               </div>
+             </div>  
+         </div>  
+    </header>
+        <!--==============================content=================================-->
+        <div id="content">
+          <div class="container">  
+            <div class="row">
+              <div class="col-lg-6 col-12">
+              	<h2>how to find us</h2>
+                <div class="map">
+                 <iframe src="http://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=Brooklyn,+New+York,+NY,+United+States&amp;aq=0&amp;sll=37.0625,-95.677068&amp;sspn=61.282355,146.513672&amp;ie=UTF8&amp;hq=&amp;hnear=Brooklyn,+Kings,+New+York&amp;ll=40.649974,-73.950005&amp;spn=0.01628,0.025663&amp;z=14&amp;iwloc=A&amp;output=embed"></iframe></div>
+                Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nulla dui. Fusce feugiat malesuada odio. Morbi nunc odio, gravida at, cursus nec, luctus a, lorem. Maecenas tristique orci ac sem. Duis ultricies pharetra magna. Donec accumsan malesuada orci. Donec sit amet eros. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Mauris fermentum dictum magna. Sed laoreet aliquam leo. Ut tellus dolor, dapibus eget, elementumve cursus eleifend, elit. 
+                <address class="address">
+                    <strong class="text-info">8901 Marmora Road, Glasgow, D04 89GR.</strong>
+                    <span>Freephone:</span>+1 800 559 6580<br>
+                    <span>Telephone:</span>+1 800 603 6035<br>
+                    <span>FAX:</span>+1 800 889 9898<br>
+                    E-mail: <a href="#" class="link">mail@demolink.org</a>
+                </address>
+              </div> 
+              <div class="col-lg-6 col-12">
+              	<h2>CONTACT</h2>
+                <form id="contact-form" class="contact-form"  style="border: solid 1px #000; padding: 10px;">
+                      <div class="success">Contact form submitted!<strong> We will be in touch soon.</strong> </div>
+                      <fieldset>
+                          <label class=" First Name">
+                           <input type="text" id="first name" value="First Name" required>						
+                           <span class="error">*This is not a valid name.</span> <span class="empty">*This field is required.</span>						
+                      </label><br>	
+                      <label class="Last name">
+                        <input type="text" id="last name" value="Last Name" required>						
+                        <span class="error">*This is not a valid name.</span> <span class="empty">*This field is required.</span>						
+                   </label><br>
+                      <label class="email">						
+                           <input type="email" id="email" value="E-mail" required>						
+                           <span class="error">*This is not a valid email address.</span> <span class="empty">*This field is required.</span>						
+                      </label><br>
+                     
+                      <label class="message">						
+                        <select name="language" id="Drop" style="width: 100%;
+                        color: #8c8b8b;
+                        border-radius: 6px;
+                        border: 1px solid #ddd;
+                        padding: 5px 8px;">
+                          <option value="Belly Dance">Hire a Belly dance</option>
+                          <option value="Corporate">Corporate</option>
+                          <option value="Classes">Classes</option>
+                          <option value="Others">Others</option>
+                        </select>			
+                   					
+                      </label>
+
+                      <div id="captchaContainer">
+                        <label for="captcha">Please enter the CAPTCHA:</label><br>
+                        <input type="text" id="captcha" name="captcha">
+                        <button onclick="generateCaptcha()">Generate CAPTCHA</button><br>
+                        <img id="captchaImage" src="" alt="CAPTCHA Image">
+                    </div>
+                    
+                    <button onclick="validateCaptcha()">Submit</button>
+                    
+                    </fieldset>
+                 
+                      <div class="pull-right55">
+                          <a href="#" class="btn" data-type="reset">clear</a>
+                          <a href="#" class="btn" data-type="submit">send message</a>
+                      </div>
+                  </form>
+              </div>
+            </div>  
+          </div>
+      </div>
+  </div>
+    <!--==============================footer=================================-->
+    <footer>
+        <div class="footer-border">
+            <div class="container">
+               <div class="row">
+                 <div class="span12">
+                    <ul class="footer-menu">
+                      <li><a href="index.html">Main</a></li>
+                      <li><a href="index-1.html">About</a></li>
+                      <li><a href="index-2.html">Teachers</a></li>
+                      <li><a href="index-3.html">Events</a></li>
+                      <li><a href="index-4.html">Videos</a></li>
+                      <li><a href="index-5.html">Presentation</a></li>
+                      <li class="active"><a href="index-6.html">Contacts</a></li>
+                    </ul>
+                 </div>
+               </div>  
+            </div>
+        </div>
+        <div class="container">
+           <div class="row">
+             <div class="span12">
+                  <div><a href="index.html" class="footer-logo">dance</a>
+                  <span>Dance &copy; 2013 &bull; <a href="index-7.html">privacy Policy</a></span></div>
+             </div>
+           </div>  
+        </div>
+    </footer>
+</div>
+
+<script>
+  function generateCaptcha() {
+      var chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+      var length = 6;
+      var captcha = "";
+      for (var i = 0; i < length; i++) {
+          captcha += chars.charAt(Math.floor(Math.random() * chars.length));
+      }
+      document.getElementById("captcha").value = captcha;
+      document.getElementById("captchaImage").src = "https://dummyimage.com/150x50/000/fff&text=" + captcha;
+  }
+  
+  function validateCaptcha() {
+      var userInput = document.getElementById("captcha").value;
+      var generatedCaptcha = document.getElementById("captchaImage").alt;
+  
+      if (userInput === generatedCaptcha) {
+          alert("CAPTCHA validated successfully!");
+          // Here you can proceed with your further actions, like form submission
+      } else {
+          alert("CAPTCHA validation failed. Please try again.");
+          // You can optionally regenerate the CAPTCHA here
+          generateCaptcha();
+      }
+  }
+  
+  // Generate CAPTCHA on page load
+  generateCaptcha();
+  </script>
+<script type="text/javascript" src="js/bootstrap.js"></script>
+
+</body>
+</html>
